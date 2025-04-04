2a604098ad4213d688d93fa272464f0a6d7c5754
felixLM <felix.lamadrid97@gmail.com>
2025-04-03 19:19:00 -0500

    feat: add pm2

---
+++ services/ui_services/ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: "ui-service",
+      script: "./src/index.js",
+      instances: 1,
+      exec_mode: "cluster",
+      watch: false,
+    },
+  ],
+};
